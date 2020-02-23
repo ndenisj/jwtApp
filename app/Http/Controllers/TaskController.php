34ddcbd6be15c21a46d27af390b41ccf423fb40a
Denis John <denis.john.ndarake@gmail.com>
2020-02-23 23:22:44 +0100

    Completed Task CRUD

---
+++ app/Http/Controllers/TaskController.php
@@ -0,0 +1,138 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Task;
+use Tymon\JWTAuth\JWTAuth;
+use Illuminate\Http\Request;
+
+class TaskController extends Controller
+{
+    protected $user;
+
+    public function __construct()
+    {
+        $this->user = JWTAuth::parseToken()->authenticate();
+    }
+
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $tasks = $this->user->tasks()->get(['title', 'description'])->toArray();
+        return $tasks;
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        $this->validate($request, [
+            'title' => 'required',
+            'description' => 'required',
+        ]);
+
+        $task = new Task();
+        $task->title = $request->title;
+        $task->description = $request->description;
+
+        if ($this->user->tasks()->save($task))
+            return response()->json([
+                'success' => true,
+                'task' => $task
+            ]);
+        else
+            return response()->json([
+                'success' => false,
+                'message' => 'Sorry, task could not be added.'
+            ], 500);
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        $task = $this->user->tasks()->find($id);
+
+        if (!$task) {
+            return response()->json([
+                'success' => false,
+                'message' => 'Sorry, task with id ' . $id . ' cannot be found.'
+            ], 400);
+        }
+
+        return $task;
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        $task = $this->user->tasks()->find($id);
+
+        if (!$task) {
+            return response()->json([
+                'success' => false,
+                'message' => 'Sorry, task with id ' . $id . ' cannot be found.'
+            ], 400);
+        }
+
+        $updated = $task->fill($request->all())->save();
+
+        if ($updated) {
+            return response()->json([
+                'success' => true
+            ]);
+        } else {
+            return response()->json([
+                'success' => false,
+                'message' => 'Sorry, task could not be updated.'
+            ], 500);
+        }
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        $task = $this->user->tasks()->find($id);
+
+        if (!$task) {
+            return response()->json([
+                'success' => false,
+                'message' => 'Sorry, task with id ' . $id . ' cannot be found.'
+            ], 400);
+        }
+
+        if ($task->delete()) {
+            return response()->json([
+                'success' => true
+            ]);
+        } else {
+            return response()->json([
+                'success' => false,
+                'message' => 'Task could not be deleted.'
+            ], 500);
+        }
+    }
+}
--- app/User.php
+++ app/User.php
@@ -57,4 +57,9 @@ class User extends Authenticatable implements JWTSubject
     {
         return [];
     }
+
+    public function tasks()
+    {
+        return $this->hasMany(Task::class);
+    }
 }
